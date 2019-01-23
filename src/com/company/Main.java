aa01fdf5269e6bb3adbfb2a115babe43011d0d5b
Angela Heredia <aheredia234@students.mchenry.edu>
2019-01-22 19:23:08 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hi, my name is Angela Heredia.");
+	System.out.println("I am 19 years old and I am finishing up my Associates in Science this semester.");
+	System.out.println("I currently work at Mariano's, which I absolutely hate.");
+	System.out.println("But its okay, because I'm starting an internship soon.");
+    }
+}
